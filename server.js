ea69ec53ae9703fa88bd0edc34e9100e3fef7b27
leokorrr <leokorrr@gmail.com>
2018-06-14 12:15:53 +0200

    app basement

---
+++ server.js
@@ -0,0 +1,17 @@
+var express = require('express');
+
+var app = express();
+
+// template engine
+app.set('view engine', 'ejs');
+
+// handle static files from this folder
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', function(req, res){
+    res.render('index')
+  });
+
+// listen to port
+app.listen(3000);
+console.log('Server is listening 3000 port');
